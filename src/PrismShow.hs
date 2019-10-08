de7fed920e4a39b71549b6da07c62ea40abbaeb4
hrttf <hrttf@ge>
2019-10-08 21:41:02 +0300

    Last "ADD" commands

--- src/PrismShow.hs
+++ src/PrismShow.hs
@@ -63,7 +63,9 @@ instructionShow = [
         makeShowFunctionAcc "ADD" 0x04 Nothing (decodeAcc8 al),
         makeShowFunctionAcc "ADD" 0x05 Nothing (decodeAcc16 ax),
         makeShowFunctionN "ADD" 0x80 (Just 0) decodeN8Imm8,
-        makeShowFunctionN "ADD" 0x81 (Just 0) decodeN16Imm
+        makeShowFunctionN "ADD" 0x81 (Just 0) decodeN16Imm,
+        makeShowFunctionN "ADD" 0x82 (Just 0) decodeN8Imm8,
+        makeShowFunctionN "ADD" 0x83 (Just 0) decodeN16Imm
     ]
 
 makeShowDecoder :: PrismDecoder
