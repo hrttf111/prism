bb0d2af6a2ad8fa5dd2a83df526aa4ca21b85a2c
admin <admin>
2019-11-22 20:41:09 +0200

    Added all string operations without prefixes

--- src/Instruction/String.hs
+++ src/Instruction/String.hs
@@ -42,7 +42,149 @@ movs16 ctx = do
 
 -------------------------------------------------------------------------------
 
+cmps8 :: Ctx -> PrismM
+cmps8 ctx = do
+    valDi <- readReg16 memReg di
+    valSi <- readReg16 memReg si
+    valMemSi <- readMem8 memReg memMain regSeg (MemDirect valSi)
+    valMemDi <- readMem8 memReg memMain (Just es) (MemDirect valDi)
+    writeReg16 memReg di (advance valDi)
+    writeReg16 memReg si (advance valSi)
+    let result = valMemSi - valMemDi
+    return $ ctx { ctxFlags = (flags result valMemDi valMemSi) }
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-1 else val+1
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+        regSeg = ctxReplaceSeg ctx
+        flags after source dest = 
+            Flags (calcCFBorrow8 dest after) (calcPF8 after) (calcAFBorrow8 dest after) (calcZF8 after) (calcSF8 after) (calcOFSub8 dest source after)
+
+cmps16 :: Ctx -> PrismM
+cmps16 ctx = do
+    valDi <- readReg16 memReg di
+    valSi <- readReg16 memReg si
+    valMemSi <- readMem16 memReg memMain regSeg (MemDirect valSi)
+    valMemDi <- readMem16 memReg memMain (Just es) (MemDirect valDi)
+    writeReg16 memReg di (advance valDi)
+    writeReg16 memReg si (advance valSi)
+    let result = valMemSi - valMemDi
+    return $ ctx { ctxFlags = (flags result valMemDi valMemSi) }
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-2 else val+2
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+        regSeg = ctxReplaceSeg ctx
+        flags after source dest = 
+            Flags (calcCFBorrow16 dest after) (calcPF16 after) (calcAFBorrow16 dest after) (calcZF16 after) (calcSF16 after) (calcOFSub16 dest source after)
+
+-------------------------------------------------------------------------------
+
+scas8 :: Ctx -> PrismM
+scas8 ctx = do
+    valAl <- readReg8 memReg al
+    valDi <- readReg16 memReg di
+    valMemDi <- readMem8 memReg memMain (Just es) (MemDirect valDi)
+    writeReg16 memReg di (advance valDi)
+    let result = valAl - valMemDi
+    return $ ctx { ctxFlags = (flags result valMemDi valAl) }
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-1 else val+1
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+        regSeg = ctxReplaceSeg ctx
+        flags after source dest = 
+            Flags (calcCFBorrow8 dest after) (calcPF8 after) (calcAFBorrow8 dest after) (calcZF8 after) (calcSF8 after) (calcOFSub8 dest source after)
+
+scas16 :: Ctx -> PrismM
+scas16 ctx = do
+    valAx <- readReg16 memReg ax
+    valDi <- readReg16 memReg di
+    valMemDi <- readMem16 memReg memMain (Just es) (MemDirect valDi)
+    writeReg16 memReg di (advance valDi)
+    let result = valAx - valMemDi
+    return $ ctx { ctxFlags = (flags result valMemDi valAx) }
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-2 else val+2
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+        regSeg = ctxReplaceSeg ctx
+        flags after source dest = 
+            Flags (calcCFBorrow16 dest after) (calcPF16 after) (calcAFBorrow16 dest after) (calcZF16 after) (calcSF16 after) (calcOFSub16 dest source after)
+
+-------------------------------------------------------------------------------
+
+lods8 :: Ctx -> PrismM
+lods8 ctx = do
+    valSi <- readReg16 memReg si
+    valMemSi <- readMem8 memReg memMain regSeg (MemDirect valSi)
+    writeReg16 memReg si (advance valSi)
+    writeReg8 memReg al valMemSi
+    return ctx
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-1 else val+1
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+        regSeg = ctxReplaceSeg ctx
+
+lods16 :: Ctx -> PrismM
+lods16 ctx = do
+    valSi <- readReg16 memReg si
+    valMemSi <- readMem16 memReg memMain regSeg (MemDirect valSi)
+    writeReg16 memReg si (advance valSi)
+    writeReg16 memReg ax valMemSi
+    return ctx
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-2 else val+2
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+        regSeg = ctxReplaceSeg ctx
+
+-------------------------------------------------------------------------------
+
+stos8 :: Ctx -> PrismM
+stos8 ctx = do
+    valDi <- readReg16 memReg di
+    valAl <- readReg8 memReg al
+    writeMem8 memReg memMain (Just es) (MemDirect valDi) valAl
+    writeReg16 memReg di (advance valDi)
+    return ctx
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-1 else val+1
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+
+stos16 :: Ctx -> PrismM
+stos16 ctx = do
+    valDi <- readReg16 memReg di
+    valAx <- readReg16 memReg ax
+    writeMem16 memReg memMain (Just es) (MemDirect valDi) valAx
+    writeReg16 memReg di (advance valDi)
+    return ctx
+    where
+        df = (eflagDF . ctxEFlags $ ctx)
+        advance val = if df then val-2 else val+2
+        memReg = ctxReg ctx
+        memMain = ctxMem ctx
+
+-------------------------------------------------------------------------------
+
 stringInstrList = [
         makeInstructionS 0xA4 Nothing (decodeImplicit movs8),
-        makeInstructionS 0xA5 Nothing (decodeImplicit movs16)
+        makeInstructionS 0xA5 Nothing (decodeImplicit movs16),
+        makeInstructionS 0xA6 Nothing (decodeImplicit cmps8),
+        makeInstructionS 0xA7 Nothing (decodeImplicit cmps16),
+        makeInstructionS 0xAA Nothing (decodeImplicit stos8),
+        makeInstructionS 0xAB Nothing (decodeImplicit stos16),
+        makeInstructionS 0xAC Nothing (decodeImplicit lods8),
+        makeInstructionS 0xAD Nothing (decodeImplicit lods16),
+        makeInstructionS 0xAE Nothing (decodeImplicit scas8),
+        makeInstructionS 0xAF Nothing (decodeImplicit scas16)
     ]
--- test-asm/TestString.hs
+++ test-asm/TestString.hs
@@ -61,3 +61,59 @@ testString env = do
                 mov bx, WORD [es:50]
                 mov ax, WORD [es:48]
             |]
+        it "CMPS8 DF=0" $ do
+            execPrism [(al `shouldEq` 1), (bl `shouldEq` 0), (di `shouldEq` 2), (si `shouldEq` 102)] env [text|
+                mov di, 0
+                mov si, 100
+                mov [si], BYTE 0xFF
+                mov [si+1], BYTE 0xAA
+                mov [es:di], BYTE 0xFF
+                mov [es:di+1], BYTE 0xAB
+                cmpsb
+                mov al, 0
+                jnz NOT_EQ
+                mov al, 1
+                NOT_EQ:
+                cmpsb
+                mov bl, 1
+                jz EQ
+                mov bl, 0
+                EQ:
+            |]
+        it "SCAS8 DF=0" $ do
+            execPrism [(bl `shouldEq` 1), (cl `shouldEq` 0), (di `shouldEq` 2)] env [text|
+                mov di, 0
+                mov [es:di], BYTE 0xFF
+                mov [es:di+1], BYTE 0xAB
+                mov al, 0xFF
+                scasb
+                mov bl, 0
+                jnz NOT_EQ
+                mov bl, 1
+                NOT_EQ:
+                scasb
+                mov cl, 1
+                jz EQ
+                mov cl, 0
+                EQ:
+            |]
+        it "lods8 DF=0" $ do
+            execPrism [(al `shouldEq` 0xAA), (bl `shouldEq` 0xFF), (si `shouldEq` 102)] env [text|
+                mov si, 100
+                mov [si], BYTE 0xFF
+                mov [si+1], BYTE 0xAA
+                mov al, 0
+                lodsb
+                mov bl, al
+                lodsb
+            |]
+        it "STOS8 DF=0" $ do
+            execPrism [(al `shouldEq` 0xFF), (bl `shouldEq` 0xAA), (di `shouldEq` 50)] env [text|
+                mov di, 48
+                mov al, 0xFF
+                stosb
+                mov al, 0xAA
+                stosb
+                mov bl, [es:49]
+                mov al, [es:48]
+            |]
