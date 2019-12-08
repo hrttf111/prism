de9a89eae0303603138d7b29e318bb88e27ac9ea
admin <admin>
2019-12-08 13:22:20 +0200

    Added GDB

---
+++ src/GDB/GDB.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses#-}
+
+module GDB.GDB where
+
+import Control.Monad.Trans (MonadTrans, MonadIO, liftIO, lift)
+import Control.Monad.Base (MonadBase, liftBase, liftBaseDefault)
+import qualified Control.Monad.State.Class as SC
+import Control.Monad.Trans.State (StateT)
+import Control.Monad.Trans.Control
+import Control.Monad.Logger
+
+import Control.Exception.Lifted (bracket)
+
+import Control.Concurrent.STM
+
+import Data.Word (Word8, Word32)
+
+import Text.Parsec
+import Text.Parsec.Text
+import qualified Data.Text as T
+
+-------------------------------------------------------------------------------
+
+data CpuCmdReq = CpuReadRegsReq 
+                 | CpuWriteReg Int Int
+                 | CpuWriteMem Int [Word8]
+                 | CpuStepReq
+                 | CpuContReq
+                 | CpuBreakReq Int
+                 | CpuReadMem Int Int deriving (Show, Eq)
+data CpuCmdRes = CpuReadRegRes [Word32] 
+                 | CpuStepRes
+                 | CpuContRes
+                 | CpuTrapRes
+                 | CpuReadMemRes [Word8] deriving (Show, Eq)
+
+data CpuMessage = CpuReq CpuCmdReq
+                  | CpuResp CpuCmdRes deriving (Show, Eq)
+
+type CmdQueue = TQueue CpuMessage
+
+data GDBState = GDBState {
+    gdbAckEnabled :: Bool,
+    gdbPacketSize :: Int,
+    gdbCmdQueueReq :: CmdQueue,
+    gdbCmdQueueRes :: CmdQueue
+}
+
+sendQueue :: MonadIO m => GDBState -> CpuMessage -> m ()
+sendQueue state msg = liftIO . atomically $ writeTQueue (gdbCmdQueueReq state) msg
+
+sendQueueAndWait :: MonadIO m => GDBState -> CpuMessage -> m CpuMessage
+sendQueueAndWait state msg = liftIO $ do
+    atomically $ writeTQueue (gdbCmdQueueReq state) msg
+    atomically $ readTQueue (gdbCmdQueueRes state)
+
+-------------------------------------------------------------------------------
+
+newtype GDBServerM m a = GDBServerM {
+    runGDB :: LoggingT (StateT GDBState m) a
+} deriving (Monad, Applicative, Functor, SC.MonadState GDBState, MonadLogger)
+
+instance MonadTrans GDBServerM where
+    lift = GDBServerM . lift . lift
+
+instance (MonadBase b m) => MonadBase b (GDBServerM m) where
+    liftBase = liftBaseDefault
+
+instance MonadIO m => MonadIO (GDBServerM m) where
+    liftIO = lift . liftIO
+
+instance MonadTransControl GDBServerM where
+    type StT GDBServerM a = StT (StateT GDBState) (StT LoggingT a)
+    liftWith = defaultLiftWith2 GDBServerM runGDB
+    restoreT = defaultRestoreT2 GDBServerM
+
+instance MonadBaseControl b m => MonadBaseControl b (GDBServerM m) where
+    type StM (GDBServerM m) a = ComposeSt GDBServerM m a
+    liftBaseWith     = defaultLiftBaseWith
+    restoreM         = defaultRestoreM
+
+type GDBServer = GDBServerM IO ()
+
+-------------------------------------------------------------------------------
---
+++ test-gdb/Spec.hs
@@ -0,0 +1,51 @@
+import Test.Hspec
+
+import Text.Parsec
+import Text.Parsec.Text
+
+import Data.Text
+import Data.Word
+
+import GDB.GDB
+import GDB.Protocol
+
+
+testHaltReason = do
+    describe "Parse HaltReason - '?'" $ do
+        it "Success" $ do
+            (parse gdbParser "" (pack "?")) `shouldBe` (Right GHaltReason)
+        it "Fail" $ do
+            (parse gdbParser "" (pack "dsdsc")) `shouldBe` (Right GUknownCommand)
+    describe "Parse Cont - 'c'" $ do
+        it "Success" $
+            (parse gdbParser "" (pack "c")) `shouldBe` (Right (GCont Nothing))
+        it "Success addr" $
+            (parse gdbParser "" (pack "c5555")) `shouldBe` (Right (GCont (Just 5555)))
+    describe "Parse ReadMem - 'm'" $ do
+        it "Success" $
+            (parse gdbParser "" (pack "m1,2")) `shouldBe` (Right (GReadMem (1,2)))
+    describe "Parse WriteMem - 'M'" $ do
+        it "Success" $
+            (parse gdbParser "" (pack "M1,2:0102")) `shouldBe` (Right (GWriteMem (1,[1,2])))
+    describe "hexConvert" $ do
+        it "toHex Word32" $ do
+            (assembleHex (0xabcd::Word32) 0) `shouldBe` "cdab0000"
+            (assembleHex (0x0bcd::Word32) 0) `shouldBe` "cd0b0000"
+            (assembleHex (0x0d::Word32) 0) `shouldBe` "0d000000"
+            (assembleHex (0x00::Word32) 0) `shouldBe` "00000000"
+        it "toHex Word16" $ do
+            (assembleHex (0xabcd::Word16) 0) `shouldBe` "cdab"
+            (assembleHex (0x0bcd::Word16) 0) `shouldBe` "cd0b"
+            (assembleHex (0x0d::Word16) 0) `shouldBe` "0d00"
+            (assembleHex (0x00::Word16) 0) `shouldBe` "0000"
+        it "toHex Word8" $ do
+            (assembleHex (0xab::Word8) 0) `shouldBe` "ab"
+            (assembleHex (0x0b::Word8) 0) `shouldBe` "0b"
+        it "fromHex Word32" $ do
+            (parse hexWordParser "" (pack "cdab0000")) `shouldBe` (Right (0xabcd :: Word32))
+            (parse hexWordParser "" (pack "00000000")) `shouldBe` (Right (0 :: Word32))
+            (parse hexWordParser "" (pack "000000")) `shouldBe` (Right (0 :: Word32))
+
+main :: IO ()
+main = do
+    hspec testHaltReason
