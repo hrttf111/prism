3d7b7608e6fffe05ca2e6c870b0582175855b820
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2023-05-11 20:41:02 +0300

    Initial bios test app

---
+++ data/bios_test.asm
@@ -0,0 +1,36 @@
+BITS 16
+CPU 8086
+
+;%macro print 2
+;%endmacro
+
+SECTION bootloader start=7C00h
+mov al, 10
+jmp 1000h:START
+times 510-($-$$) db 0
+dw 0xAA55
+
+SECTION other start=1000h
+str2 db "2322",0
+
+SECTION .data start=2000h
+str1 db "1111",0
+
+SECTION .text start=10000h
+func:
+    mov cx, 1
+    mov si, [str2+1]
+    ret
+
+START:
+mov ah, 14
+mov al, 'a'
+int 0x10
+mov ah, 14
+mov al, 'b'
+int 0x10
+mov al, [str1+2]
+call func
+mov dl, [str2+1]
+mov dl, [str2+1]
+hlt
