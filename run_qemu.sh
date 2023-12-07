3f575868a3264f1b0ac608b26ce529b0bf060fcf
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2023-12-07 21:47:47 +0200

    Added initial QEMU info

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,4 +1,8 @@
-qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img
+#qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
+#qemu-system-i386 -fda ./data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
+#qemu-system-i386 -device loader,addr=0x7c00,cpu-num=0,file=data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
+qemu-system-i386 -device loader,addr=0,cpu-num=0,file=data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
+#qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img --mem-path ./mem.data -m 1M
 #qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img -S -s &
 #gdb -ex 'target remote localhost:1234' -ex 'set disassembly-flavor intel' -ex 'set architecture i8086' -ex 'break *0x27b26'
 #gdb -ex 'target remote localhost:1234' \
