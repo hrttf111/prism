dd1ba3ddb5501d909a4ddd31c68c9ed7c336a319
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2023-12-07 22:00:07 +0200

    Some additions

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,7 +1,8 @@
 #qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
 #qemu-system-i386 -fda ./data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
 #qemu-system-i386 -device loader,addr=0x7c00,cpu-num=0,file=data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
-qemu-system-i386 -device loader,addr=0,cpu-num=0,file=data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
+#qemu-system-i386 -device loader,addr=0,cpu-num=0,file=data/bios_test -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
+qemu-system-i386 -drive file=data/bios_test1,index=0,media=disk,format=raw -boot c -m 1M -object memory-backend-file,id=pc.ram,size=1M,mem-path=mem.data,prealloc=on,share=on -machine memory-backend=pc.ram
 #qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img --mem-path ./mem.data -m 1M
 #qemu-system-i386 -fda /opt/sandbox/pc_emu/freedos/144m/x86BOOT.img -S -s &
 #gdb -ex 'target remote localhost:1234' -ex 'set disassembly-flavor intel' -ex 'set architecture i8086' -ex 'break *0x27b26'
