e2d1ad9a14a65095c8dd826e34cb7f87c7ed22fa
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2023-12-22 22:09:32 +0200

    Print assembler errors

--- test-asm/Assembler.hs
+++ test-asm/Assembler.hs
@@ -4,13 +4,14 @@ module Assembler
       makeAsmStr
     ) where
 
-import System.IO (hFlush, hClose, withFile, IOMode( ReadMode ) )
+import System.IO (hFlush, hClose, withFile, readFile', IOMode( ReadMode ) )
 import System.Exit (ExitCode( ExitSuccess ))
 
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
 
-import System.Posix.IO (stdOutput, stdError, dupTo, openFd, OpenMode(WriteOnly), defaultFileFlags)
+import System.Posix.Files (ownerReadMode, ownerWriteMode, unionFileModes)
+import System.Posix.IO (stdOutput, stdError, dupTo, openFd, closeFd, OpenMode(WriteOnly), defaultFileFlags)
 import System.Posix.Process (ProcessStatus( Exited ), executeFile, forkProcess, getProcessStatus)
 import System.Posix.Temp (mkstemp)
 import qualified Control.Exception as E
@@ -28,31 +29,38 @@ makeAsmStr text = makeAsm $ encodeUtf8 text
 
 makeAsm :: BC.ByteString -> IO B.ByteString
 makeAsm input = 
-    E.bracket mkLibFile removeLib
-        (\f1 -> E.bracket mkTempFile closeAndRemove $ m 0 f1)
+    E.bracket mkLibFile removePath
+        (\pathLib -> E.bracket mkLogFile removePath
+            ((\pathLib pathLog -> E.bracket mkTempFile closeAndRemove $ m 0 pathLib pathLog) pathLib))
     where
-        removeLib path = E.catch (removeFile path) (\(ex :: E.IOException) -> return())
+        removePath path = E.catch (removeFile path) (\(ex :: E.IOException) -> return())
         closeAndRemove (f, h) = hClose h >> removeFile f
         mkLibFile = return "/tmp/asmhs_lib"
         mkTempFile = mkstemp "/tmp/asmhs_temp_"
-        m 10 _ _ = return B.empty
-        m n pathLib pathTemp = do
-            result <- execAsm pathLib pathTemp
+        mkLogFile = return "/tmp/asmhs_log"
+        m 10 _ _ _ = return B.empty
+        m n pathLib pathLog pathTemp = do
+            result <- execAsm pathLib pathLog pathTemp
             case result of
                 Just code -> return code
-                Nothing -> m (n+1) pathLib pathTemp
-        execAsm pathLib (path, h) = do
+                Nothing -> m (n+1) pathLib pathLog pathTemp
+        execAsm pathLib pathLog (path, h) = do
             B.hPutStr h input >> hFlush h
-            (forkProcess $ openAsm path pathLib)
+            logFd <- openFd pathLog WriteOnly (Just $ unionFileModes ownerWriteMode ownerReadMode) defaultFileFlags
+            (forkProcess $ openAsm path pathLib logFd)
                 >>= getProcessStatus True True
-                >>= processStatus pathLib
-        processStatus path (Just (Exited ExitSuccess)) =
+                >>= processStatus pathLib pathLog logFd
+        processStatus path _ logFd (Just (Exited ExitSuccess)) = do
+            closeFd logFd
             Just <$> withFile path ReadMode B.hGetContents
-        processStatus _ _ = return Nothing
-        openAsm path pathLib = do
-            nullFd <- openFd "/dev/null" WriteOnly Nothing defaultFileFlags
-            dupTo nullFd stdOutput
-            dupTo nullFd stdError
+        processStatus _ pathLog logFd _ = do
+            closeFd logFd
+            content <- readFile' pathLog
+            putStrLn content
+            return Nothing
+        openAsm path pathLib logFd = do
+            dupTo logFd stdOutput
+            dupTo logFd stdError
             executeFile "yasm" True (opts path pathLib) Nothing
         opts path pathLib = ["-f", "bin", "-p", "nasm", path, "-o", pathLib]
 
